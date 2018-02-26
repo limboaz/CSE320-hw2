3ce5fe6a99ba648280f8458958f047af621e2a69
Yinuo Zhang <yinuzhang@cs.stonybrook.edu>
2018-02-26 11:17:38 -0800

    First day of work

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	gcc main.c fake_mem.o -o defrag_tool
--- main.c
+++ main.c
@@ -1,4 +1,10 @@
 #include "helper.h"
+#define MEM_MAX 1024
+
+int checkFlag(void * ptr){}
+int checkID(void * ptr, int id){}
+int checkSize(void * ptr){}
+
 
 int main(int argc, char** argv) {
     if (*(argv + 1) == NULL) {
@@ -8,11 +14,33 @@ int main(int argc, char** argv) {
     void* ram = cse320_init(*(argv + 1));
     void* tmp_buf = cse320_tmp_buffer_init();
     int ret = 0;
+    void * cursor = ram;
     /*
      * You code goes below. Do not modify provided parts
      * of the code.
      */
+    int id;
+    int af;
+    uint64_t size;
+    int mem_cnt = 0;
+    int add_flag = 0;
+    int tmp_size = 0;
+    printf("ram: %p\n", ram);
+    printf("tmp_buffer: %p\n", tmp_buf);
+    printf("seg fault: %lu\n", *((uint64_t *)ram + 256));
 
+    while(mem_cnt < MEM_MAX){
+	    //Check if ID is valid
+	for (id = 1; id < 4; id++){
+		add_flag = checkID(cursor, id);	//Match the current id loop
+		if(add_flag == 1)
+			add_flag = checkFlag(cursor);	//If add-flag is 0 at this point, this will just be skipped
+		if (add_flag == 1)
+			add_flag = checkSize(cursor, tmp_size);	//Same here
+
+	}
+    }
+	
 
     /*
      * Do not modify code below.
